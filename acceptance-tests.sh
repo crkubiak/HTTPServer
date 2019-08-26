dc744ebaac1f4f0b65ac4e3ea4b9048fccc44334
crkubiak <charles@8thlight.com>
2019-08-26 16:13:13 -0500

    .sh file corrected

--- acceptance-tests.sh
+++ acceptance-tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 gem install bundler
-wget --header="Authorization: token ${GIT_TOKEN}" https://github.com/${GIT_LOGIN}/http_server_spec/archive/master.tar.gz
+wget https://github.com/crkubiak/http_server_spec/archive/master.tar.gz
 tar -xzvf master.tar.gz
 cd http_server_spec-master && bundle install
-bundle exec spinach --tags @simple-get,@simple-head,@not-found,@simple-post,@not-allowed,@simple-redirect
+bundle exec spinach 
